cd25c6d2f79e1326ed92ac673e13a8a5e3c2d336
Yuliia <skylenko@gmail.com>
2017-12-07 16:15:49 +0200

    created linkedList project

---
+++ LinkedList/MyLinkedList.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    public class MyLinkedList<E> : IEnumerable<E>
+    {
+        private int _size;
+        private Entry<E> _first;
+        private Entry<E> _last;
+
+        internal class Entry<E>
+        {
+            public readonly E Element;
+            public Entry<E> Next;
+            public Entry<E> Prev;
+
+            public Entry(E element, Entry<E> next, Entry<E> prev)
+            {
+                Element = element;
+                Next = next;
+                Prev = prev;
+            }
+        }
+
+        public int Length()
+        {
+            return _size;
+        }
+
+        public void Add(E value)
+        {
+            Entry<E> entry = new Entry<E>(value, null, _last);
+
+            if (_last != null)
+            {
+                _last.Next = entry;
+            }
+
+            _last = entry;
+
+            if (_first == null)
+            {
+                _first = entry;
+            }
+            _size++;
+        }
+
+        public void AddAt(int index, E value)
+        {
+            Entry<E> temp = GetEntryAt(index);
+
+            if (temp == null)
+            {
+                Add(value);
+                return;
+            }
+
+            Entry<E> post = temp.Next;
+            Entry<E> pre = temp.Prev;
+            Entry<E> newEntry = new Entry<E>(value, post, temp);
+
+            if (pre != null)
+            {
+                pre.Next = newEntry;
+            }
+
+            if (post != null)
+            {
+                post.Prev = newEntry;
+            }
+            _size++;
+        }
+
+        public void Remove(E value)
+        {
+            E pointer = default(E);
+            Entry<E> temp = _first;
+            while (pointer != null && !pointer.Equals(value))
+            {
+                temp = temp.Next;
+                pointer = temp.Element;
+            }
+            Entry<E> prev = temp.Prev;
+            Entry<E> next = temp.Next;
+
+            if (prev != null)
+            {
+                prev.Next = next;
+            }
+            _size--;
+        }
+
+        public void RemoveAt(int index)
+        {
+            Entry<E> temp = GetEntryAt(index);
+            Entry<E> prev = temp.Prev;
+            Entry<E> next = temp.Next;
+
+            if (prev != null)
+            {
+                prev.Next = next;
+            }
+
+            if (next != null)
+            {
+                next.Prev = prev;
+            }
+            _size--;
+        }
+
+        public E ElementAt(int index)
+        {
+            Entry<E> temp = GetEntryAt(index);
+            if (temp == null)
+            {
+                throw new Exception("There are not elements by this index");
+            }
+            return temp.Element;
+        }
+
+        private Entry<E> GetEntryAt(int index)
+        {
+            int pointer = 0;
+            Entry<E> temp = _first;
+            while (pointer != index)
+            {
+                temp = temp.Next;
+                pointer++;
+            }
+            return temp;
+        }
+
+        public IEnumerator<E> GetEnumerator()
+        {
+            return new MyListEnumerator(_first);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class MyListEnumerator : IEnumerator<E>
+        {
+            private Entry<E> _currentEntry;
+            private readonly Entry<E> _first;
+
+            public MyListEnumerator(Entry<E> first)
+            {
+                _first = first;
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                if (_currentEntry == null)
+                {
+                    _currentEntry = _first;
+                    Current = _currentEntry.Element;
+                    return true;
+                }
+
+                if (_currentEntry.Next == null) return false;
+                _currentEntry = _currentEntry.Next;
+                Current = _currentEntry.Element;
+                return true;
+            }
+
+            public void Reset()
+            {
+                _currentEntry = _first;
+            }
+
+            public E Current { get; set; }
+
+            object IEnumerator.Current => Current;
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using LinkedList;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private MyLinkedList<string> _linkedList;
+
+        [SetUp]
+        public void Init()
+        {
+            _linkedList = new MyLinkedList<string>();
+        }
+
+        [Test]
+        public void ShouldReturnLength()
+        {
+            _linkedList.Add("test");
+            Assert.True(_linkedList.Length().Equals(1));
+        }
+
+        [Test]
+        public void ShouldAddElement()
+        {
+            _linkedList.Add("snow");
+            Assert.True(_linkedList.Length().Equals(1));
+        }
+
+        [Test]
+        public void ShouldAddElementByIndex()
+        {
+            _linkedList.AddAt(0, "tree");
+            Assert.True((_linkedList.Length().Equals(1)));
+        }
+
+        [Test]
+        public void ShouldAddSecondElementByIndex()
+        {
+            _linkedList.AddAt(0, "dog");
+            _linkedList.AddAt(1, "cat");
+            Assert.True(_linkedList.Length().Equals(2));
+        }
+
+        [Test]
+        public void ShouldAddSecondElementBySameIndex()
+        {
+            _linkedList.AddAt(0, "one");
+            _linkedList.AddAt(0, "two");
+            Assert.True(_linkedList.Length().Equals(2));
+        }
+
+        [Test]
+        public void ShouldRemoveElement()
+        {
+            _linkedList.Add("cat");
+            _linkedList.Remove("cat");
+            Assert.True(_linkedList.Length().Equals(0));
+        }
+
+        [Test]
+        public void ShouldRemoveMidleElement()
+        {
+            _linkedList.Add("spring");
+            _linkedList.Add("summer");
+            _linkedList.Add("winter");
+            _linkedList.Remove("summer");
+            Assert.True(_linkedList.Length().Equals(2));
+        }
+
+        [Test]
+        public void ShouldRemoveLastElement()
+        {
+            _linkedList.Add("spring");
+            _linkedList.Add("summer");
+            _linkedList.Add("winter");
+            _linkedList.Remove("winter");
+            Assert.True(_linkedList.Length().Equals(2));
+        }
+
+        [Test]
+        public void ShouldRemoveFirstElement()
+        {
+            _linkedList.Add("spring");
+            _linkedList.Add("summer");
+            _linkedList.Add("winter");
+            _linkedList.Remove("spring");
+            Assert.True(_linkedList.Length().Equals(2));
+        }
+
+        [Test]
+        public void ShouldRemoveElementByIndex()
+        {
+            _linkedList.Add("element");
+            _linkedList.RemoveAt(0);
+            Assert.True(_linkedList.Length().Equals(0));
+        }
+
+        [Test]
+        public void ShouldRemoveElementByFirstIndex()
+        {
+            _linkedList.Add("1");
+            _linkedList.Add("2");
+            _linkedList.Add("3");
+            _linkedList.RemoveAt(0);
+            Assert.True(_linkedList.Length().Equals(2));
+        }
+
+        [Test]
+        public void ShouldRemoveElementByMidleIndex()
+        {
+            _linkedList.Add("1");
+            _linkedList.Add("2");
+            _linkedList.Add("3");
+            _linkedList.RemoveAt(1);
+            Assert.True(_linkedList.Length().Equals(2));
+        }
+
+        [Test]
+        public void ShouldRemoveElementByLastIndex()
+        {
+            _linkedList.Add("1");
+            _linkedList.Add("2");
+            _linkedList.Add("3");
+            _linkedList.RemoveAt(2);
+            Assert.True(_linkedList.Length().Equals(2));
+        }
+
+        [Test]
+        public void ShouldShowElementByIndex()
+        {
+            _linkedList.Add("Sun");
+            var o = _linkedList.ElementAt(0);
+            Assert.AreEqual(o, "Sun");
+        }
+
+        [Test]
+        public void ShouldNotShowElementByNullIndex()
+        {
+            Exception exception = null;
+            _linkedList.Add("Live");
+            _linkedList.Remove("Live");
+
+            try
+            {
+                var o = _linkedList.ElementAt(0);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            if (exception != null) Assert.AreEqual(exception.Message, "There are not elements by this index");
+        }
+
+        [Test]
+        public void ShouldShowElementByIndexAfterChangedValue()
+        {
+            _linkedList.Add("Black");
+            _linkedList.Add("White");
+            _linkedList.AddAt(1, "Red");
+            var o = _linkedList.ElementAt(1);
+            Assert.AreEqual(o, "Red");
+        }
+
+        [Test]
+        public void ShouldWorkForeach()
+        {
+            _linkedList.Add("1");
+            _linkedList.Add("2");
+            _linkedList.Add("3");
+
+            string res = " ";
+
+            foreach (var element in _linkedList)
+            {
+                res += " " + element;
+            }
+
+            var trim = res.Trim();
+            Assert.AreEqual(trim, "1 2 3");
+        }
+    }
+}
